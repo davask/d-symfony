2b8216f300e9fab0278c26a684d074574070098a
davask <davask.42@gmail.com>
2016-09-24 15:27:20 +0200

    Update init process

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# d-symfony2
+# dockerfile
+
+see [FROM IMAGE README.md](https://github.com/davask/d-php)
+
+### Symfony activation
+
+#### comment
+
+- run /tmp/dwl/symfony.sh to reassign the correct permission
+
+## LABEL
+
+> dwl.app.framework="symfony"
---
+++ tmp/dwl/symfony.sh
@@ -0,0 +1,8 @@
+SF2PERM="/var/www/html/app/cache /var/www/html/app/logs /var/www/html/web"
+if [ -d var ]; then
+    SF2PERM="/var/www/html/var"
+fi
+
+HTTPDUSER=`ps axo user,comm | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1`
+setfacl -R -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX ${SF2PERM}
+setfacl -dR -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX ${SF2PERM}
