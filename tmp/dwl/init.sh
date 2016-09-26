23d444586d379fdba2f4ca037dc4a0a2f4480970
davask <davask.42@gmail.com>
2016-09-26 10:29:47 +0200

    Update init process

--- tmp/dwl/init.sh
+++ tmp/dwl/init.sh
@@ -12,6 +12,9 @@ echo ">> dwl conf activated";
 . /tmp/dwl/apache2.sh
 echo ">> apache2 initialized";
 
+. /tmp/dwl/mysql.sh
+echo ">> mysql setup";
+
 . /tmp/dwl/sendmail.sh
 echo ">> sendmail initialized";
 
