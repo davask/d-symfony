6c22e10ab0366efec41203b88aed5e5fd3b2e197
davask <davask.42@gmail.com>
2016-09-27 17:33:04 +0200

    Automatic update 2-p5.6-a2.4-u14.04

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,15 @@
-FROM davask/d-php:5.6-a2.4-u14.04
+FROM davask/d-php:7.0-a2.4-u16.04
 MAINTAINER davask <admin@davaskweblimited.com>
 LABEL dwl.app.framework="symfony"
+
+RUN /bin/bash -c 'apt-get update'
+RUN /bin/bash -c 'apt-get install -y php7.0-xml'
+RUN /bin/bash -c 'rm -rf /var/lib/apt/lists/*'
+
+RUN /bin/bash -c 'curl -LsS https://symfony.com/installer -o /usr/local/bin/symfony'
+RUN /bin/bash -c 'chmod a+x /usr/local/bin/symfony'
+RUN /bin/bash -c 'symfony new symfony 2.8'
+RUN /bin/bash -c 'mv symfony /tmp/dwl'
 
 COPY ./tmp/dwl/symfony.sh /tmp/dwl/symfony.sh
 COPY ./tmp/dwl/init.sh /tmp/dwl/init.sh
--- tmp/dwl/symfony.sh
+++ tmp/dwl/symfony.sh
@@ -1,8 +1,15 @@
-SF2PERM="/var/www/html/app/cache /var/www/html/app/logs /var/www/html/web"
-if [ -d var ]; then
-    SF2PERM="/var/www/html/var"
+if [ `find /var/www/html -mindepth 1 -maxdepth 1 | wc -l` = "0" ]; then
+    cp -rdf /tmp/dwl/symfony/* /home/${DWL_USER_NAME}/files;
+fi
+
+SF2PERM[0]="/var/www/html/app/cache";
+SF2PERM[1]="/var/www/html/app/logs";
+SF2PERM[2]="/var/www/html/web";
+if [ -d /var/www/html/var ]; then
+    unset SF2PERM;
+    SF2PERM[0]="/var/www/html/var"
 fi
 
 HTTPDUSER=`ps axo user,comm | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1`
-setfacl -R -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX ${SF2PERM}
-setfacl -dR -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX ${SF2PERM}
+setfacl -R -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX ${SF2PERM[@]}
+setfacl -dR -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX ${SF2PERM[@]}
