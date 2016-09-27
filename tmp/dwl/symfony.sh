a87051ad23892d5a7513917c057c3336e6ab3548
davask <davask.42@gmail.com>
2016-09-27 22:33:40 +0200

    Automatic update 3-p7.0-a2.4-u16.04

--- tmp/dwl/symfony.sh
+++ tmp/dwl/symfony.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
-SF2PERM[0]="/home/${DWL_USER_NAME:-`whoami`}/files/app/cache";
-SF2PERM[1]="/home/${DWL_USER_NAME:-`whoami`}/files/app/logs";
-SF2PERM[2]="/home/${DWL_USER_NAME:-`whoami`}/files/web";
+SF2PERM[0]="/home/${DWLC_USER_NAME}/files/app/cache";
+SF2PERM[1]="/home/${DWLC_USER_NAME}/files/app/logs";
+SF2PERM[2]="/home/${DWLC_USER_NAME}/files/web";
 if [ -d /var/www/html/var ]; then
     unset SF2PERM;
     SF2PERM[0]="/var/www/html/var"
 fi
 
 HTTPDUSER=`ps axo user,comm | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1`
-setfacl -R -m u:"$HTTPDUSER":rwX -m u:${DWL_USER_NAME:-`whoami`}:rwX ${SF2PERM[@]}
-setfacl -dR -m u:"$HTTPDUSER":rwX -m u:${DWL_USER_NAME:-`whoami`}:rwX ${SF2PERM[@]}
+setfacl -R -m u:"$HTTPDUSER":rwX -m u:${DWLC_USER_NAME}:rwX ${SF2PERM[@]}
+setfacl -dR -m u:"$HTTPDUSER":rwX -m u:${DWLC_USER_NAME}:rwX ${SF2PERM[@]}
