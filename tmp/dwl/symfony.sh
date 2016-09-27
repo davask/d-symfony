ea3a7242dcf45279faf60e7dd36c9a38353dfd33
davask <davask.42@gmail.com>
2016-09-27 16:10:48 +0200

    Automatic update 2-p7.0-a2.4-u16.04

--- tmp/dwl/symfony.sh
+++ tmp/dwl/symfony.sh
@@ -1,3 +1,7 @@
+if [ `find /var/www/html -mindepth 1 -maxdepth 1 | wc -l` = "0" ]; then
+    cp -rdf /tmp/dwl/symfony/* /home/${DWL_USER_NAME}/files;
+fi
+
 SF2PERM="/var/www/html/app/cache /var/www/html/app/logs /var/www/html/web"
 if [ -d /var/www/html/var ]; then
     SF2PERM="/var/www/html/var"
