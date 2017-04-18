3723eb8281d3acec342fa8d319983c76fb31415b
davask <contact@davaskweblimited.com>
2017-04-18 18:34:17 +0200

    Automatic update 2.8-p5.6-a2.4-u14.04

--- README.md
+++ README.md
@@ -2,13 +2,42 @@
 
 see [FROM IMAGE README.md](https://github.com/davask/d-php-letsencrypt)
 
-### SYMFONY 2.8 activation
+### SYMFONY  2.8 activation
 
 #### comment
 
-- to avoid init error all conf file in /etc/apache2/sites-available have to be in the format of sub.domain.ext.conf like docker.davaskweblimited.com.conf
+[handler_id](http://symfony.com/doc/current/reference/configuration/framework.html#handler-id)
 
-- On start all *.conf in /etc/apache2/sites-available are enabled
+> type: string default: 'session.handler.native_file'
+
+> The service id used for session storage. The session.handler service alias will be set to this service id.
+
+> You can also set it to null, to default to the handler of your PHP installation.
+
+> You can see an example of the usage of this in How to Use PdoSessionHandler to Store Sessions in the Database.
+
+The following session handler can generate issues.
+
+##### app\config\config.yml
+
+
+
+To avoid issues you can use one of these following solutions (only the default handler have been tested)
+
+##### app\config\config.yml
+
+default php handler
+
+
+
+[memcached handler](https://github.com/symfony/symfony/issues/17845#issuecomment-195285004)
+
+
+
+[PDO Handler](http://symfony.com/doc/current/doctrine/pdo_session_storage.html)
+
+
+
 
 ## LABEL
 
--- script/README.sh
+++ script/README.sh
@@ -13,13 +13,71 @@ echo "# dockerfile
 
 see [FROM IMAGE README.md](https://github.com/davask/d-php-letsencrypt)
 
-### SYMFONY ${SYMFONY_VERSION} activation
+### SYMFONY  ${SYMFONY_VERSION} activation
 
 #### comment
 
-- to avoid init error all conf file in /etc/apache2/sites-available have to be in the format of sub.domain.ext.conf like docker.davaskweblimited.com.conf
+[handler_id](http://symfony.com/doc/current/reference/configuration/framework.html#handler-id)
 
-- On start all *.conf in /etc/apache2/sites-available are enabled
+> type: string default: 'session.handler.native_file'
+
+> The service id used for session storage. The session.handler service alias will be set to this service id.
+
+> You can also set it to null, to default to the handler of your PHP installation.
+
+> You can see an example of the usage of this in How to Use PdoSessionHandler to Store Sessions in the Database.
+
+The following session handler can generate issues.
+
+##### app\config\config.yml
+
+```yml
+
+framework:
+    framework:
+        session:
+            handler_id:  session.handler.native_file
+             save_path:   "%kernel.root_dir%/../var/sessions/%kernel.environment%"
+
+```
+
+To avoid issues you can use one of these following solutions (only the default handler have been tested)
+
+##### app\config\config.yml
+
+default php handler
+
+```yml
+
+framework:
+    framework:
+        session:
+            handler_id:  ~
+
+```
+
+[memcached handler](https://github.com/symfony/symfony/issues/17845#issuecomment-195285004)
+
+```yml
+
+framework:
+    framework:
+        session:
+            # handler_id:  session.handler.memcached
+
+```
+
+[PDO Handler](http://symfony.com/doc/current/doctrine/pdo_session_storage.html)
+
+```yml
+
+framework:
+    framework:
+        session:
+            # handler_id: session.handler.pdo
+
+```
+
 
 ## LABEL
 
