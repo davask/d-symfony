2c37c65c0438f2ab8c8d1ba3e8265a6c30cca505
davask <contact@davaskweblimited.com>
2017-06-22 17:20:49 +0200

    Automatic update 2.8-p7.0-a2.4-d8.8

--- script/Dockerfile.sh
+++ script/Dockerfile.sh
@@ -24,7 +24,7 @@ RUN curl -LsS https://symfony.com/installer -o /usr/local/bin/symfony; \
 chmod a+x /usr/local/bin/symfony; \
 rm -rdf /dwl/default/var/www/html; \
 symfony new /dwl/default/var/www/symfony ${SYMFONY_VERSION:-2.8}; \
-mv /dwl/default/var/www/symfony /dwl/default/var/www/html
+cp -rdf /dwl/default/var/www/symfony /dwl/default/var/www/html
 
 COPY ./build/dwl/php.sh \
 ./build/dwl/asdf.py \
