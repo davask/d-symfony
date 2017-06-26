aad208a6fa7da2e0849a5a126df7d5ba385e988f
davask <contact@davaskweblimited.com>
2017-06-26 20:00:13 +0200

    Automatic update 2.8-p7.0-a2.4-d8.8

--- build/dwl/init.sh
+++ build/dwl/init.sh
@@ -4,46 +4,4 @@ cd .; cd -;
 
 dwlDir="/dwl";
 
-. ${dwlDir}/envvar.sh
-. ${dwlDir}/user.sh
-. ${dwlDir}/ssh.sh
-echo ">> Os initialized";
-
-echo ">> Base initialized";
-
-. ${dwlDir}/permission.sh
-echo ">> Permission assigned";
-
-. ${dwlDir}/activateconf.sh
-echo ">> Dwl conf activated";
-
-. ${dwlDir}/virtualhost.sh
-echo ">> Virtualhost generic update";
-
-. ${dwlDir}/openssl.sh
-echo ">> Openssl initialized";
-
-. ${dwlDir}/certbot.sh
-echo ">> Certbot initialized";
-
-. ${dwlDir}/virtualhost-ssl.sh
-echo ">> SSL initialized";
-
-. ${dwlDir}/virtualhost-tsl.sh
-echo ">> TSL initialized";
-
-. ${dwlDir}/apache2.sh
-echo ">> Apache2 initialized";
-
-. ${dwlDir}/php.sh
-. ${dwlDir}/sendmail.sh
-echo ">> Sendmail initialized";
-
-. ${dwlDir}/get-symfony-app.sh
-. ${dwlDir}/prepare-symfony-app.sh
-echo ">> Symfony initialized";
-
-. ${dwlDir}/fix-symfony-permissions.sh
-echo ">> fix Symfony initialized";
-
 . ${dwlDir}/keeprunning.sh
