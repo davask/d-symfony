593b8a34310f682ba127004906379559df037b9a
davask <contact@davaskweblimited.com>
2017-06-26 13:28:53 +0200

    Automatic update 2.8-p7.0-a2.4-d8.8

--- build/dwl/get-symfony-app.sh
+++ build/dwl/get-symfony-app.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
 if [ `find /home/${DWLC_USER_NAME}/files -mindepth 1 -maxdepth 1 | wc -l` == "0" ]; then
-    symfony new /home/${DWLC_USER_NAME}/files ${SYMFONY_VERSION:-2.8};
+    echo "#####################################################################################";
+    echo "# [RUN FROM BASH] symfony new /home/${DWLC_USER_NAME}/files ${SYMFONY_VERSION:-2.8} #";
+    echo "#####################################################################################";
+else
+    echo "Symfony app is not installed because there's file(s) in /home/${DWLC_USER_NAME}/files";
 fi
