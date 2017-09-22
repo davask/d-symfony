21a16241620c4f63ff627665aa6d1c8851f33f54
davask <contact@davaskweblimited.com>
2017-09-22 04:01:53 +0200

    Automatic update 2.8-p5.6-a2.4-u16.04

--- build/dwl/init.sh
+++ build/dwl/init.sh
@@ -42,5 +42,13 @@ echo ">> Php initialized";
 echo ">> fix Symfony initialized";
 
 . ${dwlDir}/custom.sh
+echo ">> custom initialized";
+
+# . ${dwlDir}/senmail.sh
+# sendmail is only available from davask/d-php*
+if [ "`dpkg --get-selections | awk '{print $1}' | grep sendmail$ | wc -l`" == "1" ]; then
+  sudo service sendmail start;
+  echo ">> Sendmail initialized";
+fi
 
 tail -f /dev/null;
